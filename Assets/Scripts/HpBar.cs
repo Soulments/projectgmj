47a9fb256b60a4a461f6d0721098d538b7500499
hyjeong99 <104803807+hyjeong99@users.noreply.github.com>
2024-07-18 23:55:52 +0900

    HP Import
    HP바 추가

---
+++ Assets/Scripts/HpBar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+    [SerializeField] GameObject goPrefab = null;
+
+    List<Transform> enemyList = new List<Transform>();
+    List<GameObject> hpBarList = new List<GameObject>();
+
+    Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+
+        GameObject[] enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemy.Length; i++) 
+        {
+            enemyList.Add(enemy[i].transform);
+            GameObject hp_bar = Instantiate(goPrefab, enemy[i].transform.position, Quaternion.identity, transform);
+            hpBarList.Add(hp_bar);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hpBarList.Count; i++)
+        {
+            hpBarList[i].transform.position = camera.WorldToScreenPoint(enemyList[i].position + new Vector3(0, 2.0f, 0));
+        }
+    }
+}
